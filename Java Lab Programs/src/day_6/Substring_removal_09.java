38d27aca1ff793b1253467457dbd7dcc79610200
surjendu104 <cyber00daddy@gmail.com>
2022-09-29 17:34:43 +0530

    Added day-6 09

--- Java Lab Programs/src/day_6/Substring_removal_09.java
+++ Java Lab Programs/src/day_6/Substring_removal_09.java
@@ -1,6 +1,6 @@
 package day_6;
 
-public class Substring_removal_08 {
+public class Substring_removal_09 {
 	public static void main(String[] args) {
 		String s = "RCC Institute of Information Technology";
 		s=s.replace("Information", "").replaceAll("\\s+", " ");
